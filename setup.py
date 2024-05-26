1fdbbfd58230d3df4499231e54736b60319e9904
eligioEC <leogionunio@gmail.com>
2024-05-26 02:58:22 +0000

    Se inicia el proyecto

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Crosseling del seguro vehicular en clientes con seguro de salud',
+    author='Eligio Espinoza Casio',
+    license='',
+)
